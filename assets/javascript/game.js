bc7df85f7e63bde2f33e4997373b371965ff1129
Rey Cruz <reycruz@Reys-MBP.attlocal.net>
2017-10-15 12:57:15 -0700

    psuedo coded steps on js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,47 @@
 // alert("welcome!");
+
+
+// global variables 
+
+
+// character variables 
+
+
+// functions 
+
+// main process 
+// onclick functions 
+$("#cersie").click(function() {
+	alert("test");
+});
+
+$("#dany").click(function() {
+	alert("test");
+});
+
+$("#snow").click(function() {
+	alert("test");
+});
+
+$("#nightking").click(function() {
+	alert("test");
+});
+
+
+
+// win count 
+
+// loss count 
+
+// if player wins 
+
+// if player losses 
+
+// start game 
+
+// restart game 
+
+// target score 
+
+// your score 
+
