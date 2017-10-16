e816264b63054d66926aaaaea024b9c40fa9b842
Rey Cruz <reycruz@Reys-MBP.attlocal.net>
2017-10-15 17:09:00 -0700

    current score changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,16 +6,30 @@
 
 // character variables 
 
-var cersie 		= 0;
-var dany 		= 0;
-var snow 		= 0; 
-var nightKing 	= 0;
+var characters = {
+	cersie:
+	{
+		value: 0
+	},
+	dany:
+	{
+		value: 0
+	},
+	snow:
+	{
+		value: 0
+	},
+	nightKing:
+	{
+		value: 0
+	},
+};
 
 
 
 // target score & your score 
 var currentScore = 0;
-var tagetScore   = 0;
+var targetScore   = 0;
 
 // win count & loss count 
 var winCount 	= 0;
@@ -25,11 +39,13 @@ var lossCount 	= 0;
 
 // functions 
 
+// get random numbers function  - works for both the target score and for each of the characters 
 var getRandomNumber = function (min,max){
 	return Math.floor(Math.random() * (max - min +1)) + min;
 
 }
 
+// function to start & restart the game 
 var startGame = function() {
 
 
@@ -40,42 +56,52 @@ var currentScore = 0;
 targetScore = getRandomNumber(19, 120)
 
 // Each crystal should have a random hidden value between 1 - 12.
-cersie 		= getRandomNumber(1,12);
-dany		= getRandomNumber(1,12);
-snow		= getRandomNumber(1,12);
-nightKing 	= getRandomNumber(1,12);
+characters.cersie.value 		= getRandomNumber(1,12);
+characters.dany.value			= getRandomNumber(1,12);
+characters.snow.value			= getRandomNumber(1,12);
+characters.nightKing.value 		= getRandomNumber(1,12);
 
 // change the HTML to reflect the changes 
-
+$("#yourScore").html(currentScore);
+$("#targetScore").html(targetScore);
 
 // test if it characters are working 
 console.log("=======================")
 console.log("Target Score: " + targetScore);
-console.log("cersie: " + cersie + " | dany: " + dany + " | snow: " + snow + " | nightking: " + nightKing);
+console.log("cersie: " + characters.cersie.value + " | dany: " + characters.dany.value + " | snow: " + characters.snow.value + " | nightKing: " + characters.nightKing.value);
 console.log("=======================")
 
 }
 
+// current score changes every time characters are clicked 
+var addScore = function(characters) {
 
+	// changes the current score 
+	currentScore = currentScore + characters.value;
+	
+
+	// testing
+	console.log("Your Score: " + currentScore);
+}
 
 
 
 // main process 
 // onclick functions 
 $("#cersie").click(function() {
-	alert("test");
+	addScore(characters.cersie);
 });
 
 $("#dany").click(function() {
-	alert("test");
+	addScore(characters.dany);
 });
 
 $("#snow").click(function() {
-	alert("test");
+	addScore(characters.snow);
 });
 
-$("#nightking").click(function() {
-	alert("test");
+$("#nightKing").click(function() {
+	addScore(characters.nightKing);
 });
 
 
